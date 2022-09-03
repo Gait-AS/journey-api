969933451ef4211ce9cb2f85645dc46555f5050d
Linus Nilsson Johnsen <linus@gait.no>
2022-09-03 03:13:08 +0200

    Started on user API and storage methods

---
+++ api/app/Http/Controllers/api/UserController.php
@@ -0,0 +1,120 @@
+<?php
+
+namespace App\Http\Controllers\api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\UserRequest;
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use function GuzzleHttp\Promise\exception_for;
+
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Database\Eloquent\Collection
+     */
+    public function index()
+    {
+        return User::all();
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(UserRequest $request)
+    {
+        $request->validated();
+
+        if (User::where('email', $request->email)->first()) {
+            return response('The user already exists', 400);
+        }
+
+        $user = new User();
+
+        $user->first_name = $request->first_name;
+        $user->last_name = $request->last_name;
+        $user->email = $request->email;
+        $user->password = Hash::make($request->password);
+
+        $user->saveOrFail();
+
+        // $user = User::create($request->validated());
+        return $user;
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param int $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param int $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @param int $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param int $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+
+    public function login(Request $request)
+    {
+        $email = $request->email;
+
+        $password = User::where('email', $email)->firstOrFail()->password;
+
+
+        // TODO: Create function that logges the user in
+        if (Hash::check($request->password, $password)) {
+            return response('Logged in', 200);
+        } else {
+            return response('Wrong email or password', 401);
+        }
+    }
+}
---
+++ api/routes/api.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::post('/login', [\App\Http\Controllers\api\UserController::class, 'login'])->name('user.authenticate ');
+Route::get('/user', [\App\Http\Controllers\api\UserController::class, 'index'])->name('user.index');
+Route::post('/user', [\App\Http\Controllers\api\UserController::class, 'store'])->name('user.create');
+//
+//Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//    return $request->user();
+//});
